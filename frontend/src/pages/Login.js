1df5fc100396d91141ef09416bd75f9e97e410c5
stoneager <139833100+stoneager@users.noreply.github.com>
2025-05-21 22:56:38 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
   const API_BASE_URL = process.env.REACT_APP_BASE_URL;
-  console.log(API_BASE_URL);
+  
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
